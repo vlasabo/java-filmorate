bca85ec0a575ed5a922a3ea7118aac1c40adb849
vlasabo <104648758+vlasabo@users.noreply.github.com>
2022-09-07 16:44:12 +0300

    feat: добавлен первый мок-тест

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -13,12 +13,23 @@ public class User {
 
     @Email(message = "EMAIL IS INCORRECT")
     @NotEmpty
-    private final String email;
+    private String email;
     @NotEmpty(message = "LOGIN IS EMPTY")
-    private final String login;
+    private String login;
     private String name;
     @Past(message = "DATE IS INCORRECT")
-    private final LocalDate birthday;
+    private LocalDate birthday;
+
+    public User(String email, String login, String name, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public User() {
+
+    }
 
 
 }
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,54 @@
 package ru.yandex.practicum.filmorate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.model.User;
 
-@SpringBootTest
-class FilmorateApplicationTests {
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class FilmorateApplicationTests {
+
+	private final LocalDate BIRTHDAY_DATE = LocalDate.ofEpochDay(1);
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@SpyBean
+	private UserController userController;
+
 
 	@Test
-	void contextLoads() {
-	}
+	@Order(1)
+	void givenNewUserWithNullName_whenCreated_thenAddsUserWithNameEqualsLogin() throws Exception {
+		User user = new User("aa@bb.com", "login", null, BIRTHDAY_DATE);
+		String requestBody = objectMapper.writeValueAsString(user);
 
+		var response = this.mockMvc.perform(post("/users")
+				.content(requestBody)
+				.contentType(MediaType.APPLICATION_JSON));
+
+		response.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.email").value("aa@bb.com"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.login").value("login"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("login"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.birthday").value(BIRTHDAY_DATE.toString()));
+	}
 }
